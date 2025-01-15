8b55621624c10f8260e4ed6aafcb23d72b350722
Daniel Mouris <dgmouris@gmail.com>
2025-01-15 16:51:51 -0700

    Added example from day 2 not complete

--- 02-review-astronauts-example-COMPLETE/js/api/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/api/astronaut.js
@@ -1,3 +1,26 @@
-const BASE_URL = "https://lldev.thespacedevs.com/2.2.0"
+// change the base url to the new version 2.3.0
+const BASE_URL = "https://lldev.thespacedevs.com/2.3.0"
 
 // api functions here.
+// create an async function that will
+// return the data fetched from the url we tested
+// export that function (named)
+// import it index.js
+const getAstronautList = async () => {
+  // construct the url.
+  const URL = `${BASE_URL}/astronauts/?mode=list`
+  // making the fetch request
+  const response = await fetch(URL, {
+    method: "GET" // this is extra data associated with fetch
+    // this would be where you put headers
+    // and also the body (if it were a POST/PATCH/PUT) req
+  })
+  // get the data from the response.json()
+  // note response.json() is a promise which is why
+  // we await it
+  const data = await response.json()
+
+  return data
+}
+
+export { getAstronautList }
--- 02-review-astronauts-example-COMPLETE/js/index.js
+++ 02-review-astronauts-example-COMPLETE/js/index.js
@@ -1,1 +1,17 @@
-// js here.
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { getAstronautList } from './api/astronaut'
+
+/*
+Take a look at the space devs api
+
+The base domain of the api endpoint is going to
+be here https://lldev.thespacedevs.com
+
+*/
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let data = await getAstronautList()
+  // get the name of tommy pesquet
+  console.log(data.results[0].name)
+})
