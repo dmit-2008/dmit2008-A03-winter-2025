0937e182b9e6dbb0464a9a9a8f38a273511de2f7
Daniel Mouris <dgmouris@gmail.com>
2025-01-20 11:19:54 -0700

    day 3 progress

--- 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
@@ -13,8 +13,27 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
 </li>
 */
 
-// create function called getAstronautListItem
+// create function called renderAstronautListItem
 // two parameters/arguments
 // use the data from the object in the html
 // above.
 // export and import in our index.js
+const renderAstronautListItem = (astronautData, element) => {
+  // the element we are just going to append innerHTML of it.
+  element.innerHTML += `<li href="#" class="list-group-item list-group-item-action">
+      <div class="d-flex w-100 justify-content-between">
+          <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">
+          <h5 class="mb-1">ASTRONAUT NAME (ASTRONAUT STATUS HERE)</h5>
+          <small class="float-end">born DATE OF BIRTH HERE</small>
+      </div>
+      <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>
+      <p class="mb-1">ASTRONAUT BIO HERE </p>
+  </li>`
+}
+
+// a named export
+export { renderAstronautListItem }
+
+// a note here if you wanted to create this a default export you would use
+// the following syntax
+// export default getAstronautListItem
