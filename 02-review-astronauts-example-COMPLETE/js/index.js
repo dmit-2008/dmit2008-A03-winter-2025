26e5bf02d856ecb272a1d94e06cd3fa60054c312
Daniel Mouris <dgmouris@gmail.com>
2025-01-22 15:41:12 -0700

    class progress pushed

--- 02-review-astronauts-example-COMPLETE/js/index.js
+++ 02-review-astronauts-example-COMPLETE/js/index.js
@@ -26,9 +26,23 @@ be here https://lldev.thespacedevs.com
 // we're also going to clearn the innerhtml of the container element
 
 // let's use our knowledge of object destructuring in the parameter
-const getAndRenderAstronautList = ({search}) => {
+const getAndRenderAstronautList = async ({search}) => {
+  // i'm going to make this parameter optional.
   console.log(search)
 
+  let data = await getAstronautList()
+  // get the name of tommy pesquet
+  console.log(data)
+  console.log(data.results[0].name)
+  // we can see in the above data that resulsts is an array
+  // so what are we going to do here.
+  let astronauts = data.results
+  // let's select the element
+  let astronautListItem = document.querySelector(".astronaut-list")
+  astronauts.map((astronaut)=> { // you could use foreach as well.
+    // we're going to call our function here
+    renderAstronautListItem(astronaut, astronautListItem)
+  })
 }
 
 // select the form, add event listener and get search value from
@@ -46,20 +60,12 @@ searchForm.addEventListener("submit", (event)=> {
   let searchInput = searchForm.elements['search']
   // let's make sure we have the correct input
   console.log(searchInput.value)
+  // let's call our function here to get and render the astronauts
+  getAndRenderAstronautList({
+    search: searchInput.value
+  })
 })
 
 document.addEventListener("DOMContentLoaded", async () => {
-  let data = await getAstronautList()
-  // get the name of tommy pesquet
-  console.log(data)
-  console.log(data.results[0].name)
-  // we can see in the above data that resulsts is an array
-  // so what are we going to do here.
-  let astronauts = data.results
-  // let's select the element
-  let astronautListItem = document.querySelector(".astronaut-list")
-  astronauts.map((astronaut)=> { // you could use foreach as well.
-    // we're going to call our function here
-    renderAstronautListItem(astronaut, astronautListItem)
-  })
+  await getAndRenderAstronautList()
 })
