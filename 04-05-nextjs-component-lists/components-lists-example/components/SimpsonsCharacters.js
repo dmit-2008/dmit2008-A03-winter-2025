919d99c59b3682213cb66c460a4308b43a659819
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:42:37 -0700

    class progress pushed

--- 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
+++ 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
@@ -9,9 +9,10 @@ const SIMPSON_CHARACTERS = [
 	"Milhouse Van Houten",
 	"Ned Flanders",
 ]
-
+// I want you to use this in the index.js and I want you to
+// loop through the list and show each item
+// with a specific key on the list item.
 export default function SimpsonsCharacter() {
-
   return <ul>
 
   </ul>
