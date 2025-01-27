95bf18efb494f8bd7d9dba04db07bc2efae299d7
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:23:24 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-intro/components/ConceptItem.js
+++ 04-05-nextjs-components-intro/components-intro/components/ConceptItem.js
@@ -2,3 +2,14 @@
 // I want you to create a ConceptItem
 // component with on prop called idea
 // that returns a list item.
+export default function ConceptItem({idea, color}) {
+  // let's use the style object to change
+  // the color, let's pass this as a prop.
+  if (!color) {
+    color = "yellow"
+  }
+  // the style prop modifies css
+  return <li style={{color: color}}>
+    {idea}
+  </li>
+}
