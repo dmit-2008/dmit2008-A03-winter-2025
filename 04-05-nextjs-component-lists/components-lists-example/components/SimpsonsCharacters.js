57a8f1cd55b1f4c77e6988ec192280edbf401359
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:46:47 -0700

    class progress pushed

--- 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
+++ 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
@@ -12,8 +12,13 @@ const SIMPSON_CHARACTERS = [
 // I want you to use this in the index.js and I want you to
 // loop through the list and show each item
 // with a specific key on the list item.
-export default function SimpsonsCharacter() {
-  return <ul>
-
-  </ul>
+export default function SimpsonsCharacters() {
+  return <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+    {/* This is the important part */}
+    {SIMPSON_CHARACTERS.map((character, index)=> {
+      // return the JSX
+      // the key needs to be unique among siblings
+      return <li key={index}>{character}</li>
+    })}
+  </ol>
 }
--- 04-05-nextjs-component-lists/components-lists-example/pages/index.js
+++ 04-05-nextjs-component-lists/components-lists-example/pages/index.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import SimpsonsCharacters from "@/components/SimpsonsCharacters";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -17,14 +19,8 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+
+        <SimpsonsCharacters />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
