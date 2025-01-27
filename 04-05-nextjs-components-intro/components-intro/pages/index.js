99f070cf1a5596da6ec861ad303494edceb3ee3a
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:19:04 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-intro/pages/index.js
+++ 04-05-nextjs-components-intro/components-intro/pages/index.js
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 // import our title component
 import Title from '@/components/Title'
+import ConceptList from "@/components/ConceptList";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,10 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Title
-          text={"Our first component"}
-        />
+        <Title text={"Our first component"} />
+        <ConceptList>
+          <li> Object destructuring </li>
+        </ConceptList>
       </main>
     </div>
   );
