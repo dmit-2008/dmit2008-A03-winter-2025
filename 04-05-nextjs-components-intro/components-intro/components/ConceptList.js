17d10bedd459e7a8359a6734586854f08d0ef3a7
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:16:38 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-intro/components/ConceptList.js
+++ 04-05-nextjs-components-intro/components-intro/components/ConceptList.js
@@ -1,8 +1,20 @@
 // I want you folks
 // create a component called
 // concept list that will be an
-// ordered list that wrap all
-// nested components
+export default function ConceptList({children}) {
+  // a note on component names, they should always be the same as the file
+  // ordered list that wrap all
+  // nested components
+  return <ol>
+    {/* this prop will allow us to
+      include any components within the opening and closing tags.
+      <ConceptList>
+        shtuff
+      </ConceptList>
+    */}
+    {children}
+  </ol>
+}
 // export default this component
 // import it in your js.
 
