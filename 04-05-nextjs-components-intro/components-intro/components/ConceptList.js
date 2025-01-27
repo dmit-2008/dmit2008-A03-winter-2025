c23dbf57a89a9efd73ea22f4c22cf50163ef2756
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:09:42 -0700

    class progress pushed

---
+++ 04-05-nextjs-components-intro/components-intro/components/ConceptList.js
@@ -0,0 +1,12 @@
+// I want you folks
+// create a component called
+// concept list that will be an
+// ordered list that wrap all
+// nested components
+// export default this component
+// import it in your js.
+
+// if you've completed the above
+// I want you to create a ConceptItem
+// component with on prop called idea
+// that returns a list item.
