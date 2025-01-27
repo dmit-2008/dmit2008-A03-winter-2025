ef7fc14a421f7e2ebe6c0eddccf600ac720c3942
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 11:38:17 -0700

    class progress pushed

---
+++ 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
@@ -0,0 +1,18 @@
+const SIMPSON_CHARACTERS = [
+	"Homer Simpson",
+	"Bart Simpson",
+	"Marge Simpson",
+	"Mr. Burns",
+	"Lisa Simpson",
+	"Apu Nahasapeemapetilon",
+	"Sideshow Bob",
+	"Milhouse Van Houten",
+	"Ned Flanders",
+]
+
+export default function SimpsonsCharacter() {
+
+  return <ul>
+
+  </ul>
+}
