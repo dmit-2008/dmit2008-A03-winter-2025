08655c7bc8c6b30a03d63eca6096d26aec29ed59
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 16:26:12 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -9,12 +9,13 @@
 // bottom of imports is your custom imports
 
 // all of these should be in alphabetical
-
+import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
-import UILibraryList from '@/components/UILibraryList';
+import UILibraryList from '@/components/UILibraryList'
 
 export default function Home() {
   return (
@@ -33,6 +34,23 @@ export default function Home() {
               <h2>Other functionality</h2>
               {/* Add a button on here with variant of danger
               I want you to add an accordion. */}
+              <Button variant="danger">
+                Don't click me.
+              </Button>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Learning #1</Accordion.Header>
+                  <Accordion.Body>
+                    React bootstrap pretty easy to use
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Learning #2</Accordion.Header>
+                  <Accordion.Body>
+                    There's a whole bunch of libraries out there.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
             </Col>
           </Row>
         </Container>
