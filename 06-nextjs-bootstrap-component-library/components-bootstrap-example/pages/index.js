c0c73d917972c66eb942615a64da0c311d3ce4e9
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 15:47:49 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -3,11 +3,27 @@
 // an h1 with some title
 // row component and col in there.
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 export default function Home() {
   return (
     <div>
       <main>
-
+        {/* Use the the container in the JSX */}
+        <Container>
+          <h1>React Bootstrap Demo</h1>
+          <Row>
+            <Col>
+              <h2>Available UI Libraries</h2>
+            </Col>
+            <Col>
+              <h2>Other functionality</h2>
+            </Col>
+          </Row>
+        </Container>
       </main>
     </div>
   );
