47edbe94f1dcbf42f9a49265a7626d88d625e649
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 16:13:15 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -3,10 +3,18 @@
 // an h1 with some title
 // row component and col in there.
 
+// let's talk about imports
+// top of imports is framework imports
+// middle of imports is third libraries say for eg react-bootstrap
+// bottom of imports is your custom imports
+
+// all of these should be in alphabetical
+
+import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
+import UILibraryList from '@/components/UILibraryList';
 
 export default function Home() {
   return (
@@ -18,6 +26,8 @@ export default function Home() {
           <Row>
             <Col>
               <h2>Available UI Libraries</h2>
+              {/* We're going to use our own component here */}
+              <UILibraryList />
             </Col>
             <Col>
               <h2>Other functionality</h2>
