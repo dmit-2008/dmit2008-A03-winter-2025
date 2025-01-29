b3c5c5598be0c758fd982f88f347fd12591bb26a
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 16:01:24 -0700

    class progress pushed

---
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/components/UILibraryList.js
@@ -0,0 +1,23 @@
+// what I want you to do
+// create a component with the same name as the file.
+// use react bootstrap list group to render this list
+// as links in the components.
+
+const UI_LIBRARY_LIST = [
+	{
+		name: "MUI",
+		url: "https://mui.com/"
+	},
+	{
+		name: "React Bootstrap (the one we're looking at)",
+		url: "https://react-bootstrap.github.io/"
+	},
+	{
+		name: "Ant Design",
+		url: "https://ant.design/docs/react/introduce"
+	},
+	{
+		name: "Semantic UI",
+		url: "https://react.semantic-ui.com/"
+	}
+]
