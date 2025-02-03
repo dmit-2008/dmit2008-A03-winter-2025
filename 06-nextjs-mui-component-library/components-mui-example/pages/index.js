c6c93d393e1b70ea9d1a03d4e405f47f34ccf03e
Daniel Mouris <dgmouris@gmail.com>
2025-02-03 10:52:00 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/pages/index.js
+++ 06-nextjs-mui-component-library/components-mui-example/pages/index.js
@@ -1,18 +1,31 @@
 // import a container
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 // use this container by taking a look documentation
+
 // take at the "component" prop and use it.
 // use typography give this a title
 
 export default function Home() {
   return (
     <div>
-      <main>
-        <Container>
+      {/*
+        Observing the api here: https://mui.com/material-ui/api/container/
+        this will render my Container as a main component.
+      */}
+      <Container
+        component="main"
+      >
+        <Typography
+          variant="h3"
+          component="h1"
+        >
+          Using MUI
+        </Typography>
 
-        </Container>
-      </main>
+      </Container>
+
     </div>
   );
 }
