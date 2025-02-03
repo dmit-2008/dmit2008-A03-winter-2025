4328310f25e04938b1af86a6d91a90b202354435
Daniel Mouris <dgmouris@gmail.com>
2025-02-03 11:34:28 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
+++ 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
@@ -1,3 +1,13 @@
+// import quite a few for the checkbox
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+
+
 // create a component
 // add two inputs
 // one that's text
@@ -5,6 +15,15 @@
 // add button
 export default function SimpleForm() {
   return <form>
-
+    <Typography variant="h6">
+      A small demo of forms
+    </Typography>
+    <TextField
+      label="a demo label"
+      variant="standard"
+    />
+    <FormGroup>
+    {/* allows to make a compact layout */}
+    </FormGroup>
   </form>
 }
