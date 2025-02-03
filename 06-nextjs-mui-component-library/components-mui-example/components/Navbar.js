122931118ac62a549d0cf13c3aa449b8376ed4ee
Daniel Mouris <dgmouris@gmail.com>
2025-02-03 11:07:47 -0700

    class progress pushed

---
+++ 06-nextjs-mui-component-library/components-mui-example/components/Navbar.js
@@ -0,0 +1,6 @@
+// I want you to create a component with the same
+// name as the file
+// give it a single prop called title
+
+// I want to use a very appbar from mui.
+// if there are styling issues, why?
