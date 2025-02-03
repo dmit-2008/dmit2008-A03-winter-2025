32bc3155f939b63dcf3336d68d1330a92e9bbadd
Daniel Mouris <dgmouris@gmail.com>
2025-02-03 11:41:09 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
+++ 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
@@ -1,12 +1,11 @@
 // import quite a few for the checkbox
+import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-
-
 
 // create a component
 // add two inputs
@@ -23,7 +22,29 @@ export default function SimpleForm() {
       variant="standard"
     />
     <FormGroup>
-    {/* allows to make a compact layout */}
+      {/* allows to make a compact layout
+        note that in the form control label
+        one of the props is actually just jsx.
+
+      */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            defaultChecked
+          />
+        }
+        label="Check me"
+      />
     </FormGroup>
+    {/*
+      Take a look here for more docs
+      https://mui.com/material-ui/react-button/
+    */}
+    <Button
+      variant="contained"
+      color="success"
+    >
+      Submit the form!
+    </Button>
   </form>
 }
