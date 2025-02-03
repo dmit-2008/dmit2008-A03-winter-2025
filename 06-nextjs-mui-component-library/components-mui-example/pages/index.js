d476d032cd2d392567f9ed28c417f21e2ef0c8ca
Daniel Mouris <dgmouris@gmail.com>
2025-02-03 10:37:45 -0700

    class progress pushed

---
+++ 06-nextjs-mui-component-library/components-mui-example/pages/index.js
@@ -0,0 +1,14 @@
+// import a container
+// use this container by taking a look documentation
+// take at the "component" prop and use it.
+// use typography give this a title
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+
+      </main>
+    </div>
+  );
+}
