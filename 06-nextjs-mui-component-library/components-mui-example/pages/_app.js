bd8ce216802fa6289e44095e967f402b4a7a2a9f
Daniel Mouris <dgmouris@gmail.com>
2025-02-05 15:45:08 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/pages/_app.js
+++ 06-nextjs-mui-component-library/components-mui-example/pages/_app.js
@@ -6,6 +6,14 @@ import '@fontsource/roboto/700.css';
 
 import "@/styles/globals.css";
 
+// we're going to import the ThemeProvider
+import { ThemeProvider } from '@mui/material/styles';
+import {theme} from '@/utils/theme/config'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // we're going to wrap the component in our themeprovider which
+  // will run on every page.
+  return <ThemeProvider theme={theme}>
+    <Component {...pageProps} />;
+  </ThemeProvider>
 }
