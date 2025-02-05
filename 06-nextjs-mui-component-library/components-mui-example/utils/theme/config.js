78a2875cb24f29d24117cdab1d88598bbfa3d105
Daniel Mouris <dgmouris@gmail.com>
2025-02-05 15:40:00 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/utils/theme/config.js
+++ 06-nextjs-mui-component-library/components-mui-example/utils/theme/config.js
@@ -3,7 +3,18 @@ import { createTheme } from '@mui/material/styles';
 
 // create some theme options that are going to change
 // the color of the primary
+const themeOptions = {
+  palette: {
+    primary: {
+      main: '#7f0672',
+    },
+    secondary: {
+      main: '#27abb0',
+    },
+  },
+}
 
 // createTheme
-
+const theme = createTheme(themeOptions)
 // export it
+export { theme }
