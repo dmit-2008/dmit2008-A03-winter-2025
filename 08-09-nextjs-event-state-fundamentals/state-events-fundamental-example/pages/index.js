6bf84ef1d4d4a8718e91a31558c42bbbe9847fb1
Daniel Mouris <dgmouris@gmail.com>
2025-02-10 11:15:49 -0700

    class progress pushed

--- 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
+++ 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
@@ -1,11 +1,26 @@
+/*
+make a container that has a form
+in that form you use an mui grid (version 2 plz)
+create a text field and a button.
 
+if you're stuck take a look at the MUI docs!
+
+if you're done this create a stateful variable for todoValue.
+*/
+import Container from '@mui/material/Container';
 
 export default function Home() {
   return (
     <div>
-      <main>
+      <Container
+        maxWidth="lg"
+        component={"main"}
+      >
+        {/* we're going to hook into this shortly. */}
+        <form>
 
-      </main>
+        </form>
+      </Container>
     </div>
   );
 }
