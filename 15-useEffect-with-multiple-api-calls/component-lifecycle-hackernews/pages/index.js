402324784c8200cb8b163b5c787718307f91f0d9
Daniel Mouris <dgmouris@gmail.com>
2025-03-17 11:42:32 -0600

    class progress pushed

--- 15-useEffect-with-multiple-api-calls/component-lifecycle-hackernews/pages/index.js
+++ 15-useEffect-with-multiple-api-calls/component-lifecycle-hackernews/pages/index.js
@@ -1,13 +1,16 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
+// Import our top stories
+import TopStoriesList from '@/components/TopStoriesList';
+
 export default function Home() {
   return (
     <Container component="main" sx={{marginTop: 3}} maxWidth="lg">
       <Typography variant='h1'>
         Top Hackernews Stories
       </Typography>
-
+      <TopStoriesList />
     </Container>
   );
 }
