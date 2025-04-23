e8b6798d8426dd6f4a4410d391a6148a6a6b16a0
Daniel Mouris <dgmouris@gmail.com>
2025-04-23 16:13:43 -0600

    class progress pushed

--- 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/state/AuthProvider.js
+++ 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/state/AuthProvider.js
@@ -1,8 +1,22 @@
 import {useState, useContext, createContext} from 'react'
 
+// 4. I'm going to import the login
+import { login } from '@/utils/api/auth'
 
 // 2. let's create the context.
 export const AuthContext = createContext({})
+
+// 7. let's create the hook to simplify
+export function useAuth() {
+  // so you can call your own hooks inside of your custom hook
+  // useEffect, useState, useRouter.
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error(`useAuth must be used within a AuthProvider`)
+  }
+  return context
+}
+
 
 // 1. let's create the component
 export default function AuthProvider({children}) {
@@ -13,14 +27,27 @@ export default function AuthProvider({children}) {
   const [user, setUser] = useState()
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
+  // 5. create the sign method
+  // we're going to create the method that is going to call the backend
+  // and update the state of the frontend and keep the values up to date
+  const signIn = async ({email, password}) => {
+    // call the backend
+    const data = await login({email: email, password: password})
 
+    // update the frontend
+    setToken(data.accessToken)
+    // update the user
+    setUser(data.user)
+    // update the state that the user isAuthenticated
+    setIsAuthenticated(true)
+  }
 
 
   // return the children in the provider
   // so in react 19.1 AuthContext.Provider has just become AuthContext
   return <AuthContext.Provider value={{
-    // this going to hold the values that other components will use.
-
+    // 6. this going to hold the values that other components will use.
+    signIn, token, user, isAuthenticated
   }}>
     {children}
   </AuthContext.Provider>
