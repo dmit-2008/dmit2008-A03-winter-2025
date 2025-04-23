4fee8ff4d8f90b357a1261acc8b431c1ceeba66d
Daniel Mouris <dgmouris@gmail.com>
2025-04-23 16:08:51 -0600

    class progress pushed

--- 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/state/AuthProvider.js
+++ 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/state/AuthProvider.js
@@ -1,11 +1,27 @@
+import {useState, useContext, createContext} from 'react'
 
 
+// 2. let's create the context.
+export const AuthContext = createContext({})
 
 // 1. let's create the component
 export default function AuthProvider({children}) {
+  // 3. creqate hte values we're going to use that are stateful
+  // this state once we set it will be accessible once passed in the
+  // the "value" of the AuthContext.Provider
+  const [token, setToken] = useState()
+  const [user, setUser] = useState()
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-  // return the children
-  return <>
+
+
+
+  // return the children in the provider
+  // so in react 19.1 AuthContext.Provider has just become AuthContext
+  return <AuthContext.Provider value={{
+    // this going to hold the values that other components will use.
+
+  }}>
     {children}
-  </>
+  </AuthContext.Provider>
 }
--- 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/pages/_app.js
+++ 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/pages/_app.js
@@ -3,10 +3,12 @@ import '@/styles/globals.css'
 // we're goign to wrap the app with
 // with the appNotification
 import AppNotification from '@/components/state/AppNotification'
+import AuthProvider from '@/components/state/AuthProvider'
 
 export default function App({ Component, pageProps }) {
   return <AppNotification>
-    <Component {...pageProps} />
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   </AppNotification>
-
 }
