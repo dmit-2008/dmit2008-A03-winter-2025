572beb378f206d1d4ffd738c6a30fc347292cd01
Daniel Mouris <dgmouris@gmail.com>
2025-04-23 15:51:14 -0600

    class progress pushed

--- 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/LoginForm.js
+++ 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/LoginForm.js
@@ -8,18 +8,30 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
-
+// use our notification hook so that
+// we can just display a message
+import { useNotification } from './state/AppNotification';
 
 export default function LoginForm(){
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const router = useRouter()
 
+  // we're going to initialize our own hookc
+  // we can destructure the value.
+  const {showNotification} = useNotification()
+  // this is the same as doing
+  // const {showNotification} = useContext(AppNotificationContext)
+
+
   /* import the hook here */
-  
   const handleLogin = (event)=> {
     event.preventDefault()
     /* make the sign in request here. */
+    showNotification({
+      message: "Login attempt",
+      severity: "error"
+    })
   }
 
   return <Box component="form" noValidate sx={{ mt: 1 }}
--- 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/state/AppNotification.js
+++ 27-authentication-example-START/authentication-example-START/app-state-auth-example-new/components/state/AppNotification.js
@@ -3,9 +3,13 @@ import { useState, useContext, createContext  } from 'react'
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 
-
+// 3. You're going to create a context
+//    we'll make it so that other components can import this.
 export const AppNotificationContext = createContext({})
 
+// 5. creating our own hoook (optional) is really nice
+//    because we only need to import one thing rather than
+//    useContext and the context itself.
 export function useNotification() {
   const context = useContext(AppNotificationContext)
   if (!context) {
@@ -15,7 +19,7 @@ export function useNotification() {
 }
 
 // 1. create a component that will wrap the entire application
-
+// 2. Import this in your _app and then you'll wrap the component in it.
 export default function AppNotification(props) {
     const [open, setOpen] = useState(false);
     const [text, setText] = useState('')
@@ -35,12 +39,16 @@ export default function AppNotification(props) {
         setOpen(true)
     }
 
+    // 4. You wrap your own component with AppNotificationContext.Provider
+    //    and pass in a value that will update the state of your application
     return <AppNotificationContext.Provider value={{showNotification}}>
       {props.children}
       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
         <MuiAlert
           elevation={6} variant="filled"
-          onClose={handleClose} severity={severity} sx={{ width: '100%' }}
+          onClose={handleClose}
+          severity={severity}
+          sx={{ width: '100%' }}
         >
           {text}
         </MuiAlert>
